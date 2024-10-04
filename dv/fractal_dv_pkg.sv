ef86e614a20113dc8eeda5e7dea6a5521d28fa7a
Victor Isachi <66256812+VictorIsachi@users.noreply.github.com>
2024-10-04 16:44:44 +0200

    Create fractal_dv_pkg.sv

---
+++ dv/fractal_dv_pkg.sv
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2023-2024 ETH Zurich and University of Bologna
+ *
+ * Licensed under the Solderpad Hardware License, Version 0.51 
+ * (the "License"); you may not use this file except in compliance 
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * SPDX-License-Identifier: SHL-0.51
+ *
+ * Authors: Victor Isachi <victor.isachi@unibo.it>
+ *
+ * Fractal synchronization verification package
+ */
+
+package fractal_dv_pkg;
+
+  // Maximul allowed distance from the desired synchronization level in a synchronization with error
+  localparam int unsigned MAX_ERROR = 3;
+  
+  `include "sync_transaction.sv"
+  `include "cu_bfm.sv"
+
+endpackage: fractal_dv_pkg
