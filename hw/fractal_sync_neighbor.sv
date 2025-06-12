a635370b6272b250929026a51060e4da68556750
Victor Isachi <victor.isachi@unibo.it>
2025-06-12 12:00:21 +0200

    Reworked neighbor node to adapt to the RF managed back-routing interface

--- hw/fractal_sync_neighbor.sv
+++ hw/fractal_sync_neighbor.sv
@@ -45,13 +45,22 @@ module fractal_sync_neighbor
 );
 
 /*******************************************************/
+/**        Parameters and Definitions Beginning       **/
+/*******************************************************/
+
+  localparam int unsigned NBR_ID_W = 2;
+  
+/*******************************************************/
+/**           Parameters and Definitions End          **/
+/*******************************************************/
 /**             Internal Signals Beginning            **/
 /*******************************************************/
 
-  logic[N_PORTS-1:0] sync_req;
-  logic              clear_sync_req;
-  logic[N_PORTS-1:0] sync_present_d, sync_present_q;
-  logic              wake;
+  logic[N_PORTS-1:0]  sync_req;
+  logic               clear_sync_req;
+  logic[N_PORTS-1:0]  sync_present_d, sync_present_q;
+  logic               wake;
+  logic[NBR_ID_W-1:0] id_d, id_q, id;
 
 /*******************************************************/
 /**                Internal Signals End               **/
@@ -60,28 +69,33 @@ module fractal_sync_neighbor
 /*******************************************************/
 
   for (genvar i = 0; i < N_PORTS; i++) begin: gen_sync_req_rsp
-    assign sync_req[i]    = req_i[i].sync;
-    assign rsp_o[i].wake  = wake;
-    assign rsp_o[i].dst   = '0;
-    assign rsp_o[i].error = 1'b0;
+    assign sync_req[i]      = req_i[i].sync;
+    assign rsp_o[i].wake    = wake;
+    assign rsp_o[i].sig.lvl = 1'b1;
+    assign rsp_o[i].sig.id  = id;
+    assign rsp_o[i].error   = 1'b0;
   end
 
   assign clear_sync_req = &sync_present_q;
 
   assign sync_present_d = sync_present_q | sync_req;
+  // No id checking is done: it is assumed that both requests will have the same id (this should be enforced by the tile-FS interface module)
+  assign id_d           = req_i[0].sig.id;
 
-  always_ff @(posedge clk_i, negedge rst_ni) begin: presence_tracker
-    if (!rst_ni)          sync_present_q <= '0;
+  always_ff @(posedge clk_i, negedge rst_ni) begin: presence_id_tracker
+    if (!rst_ni)          begin sync_present_q <= '0;             id_q <= 0;    end
     else begin
-      if (clear_sync_req) sync_present_q <= '0;
-      else                sync_present_q <= sync_present_d;
+      if (clear_sync_req) begin sync_present_q <= '0;             id_q <= 0;    end
+      else                begin sync_present_q <= sync_present_d; id_q <= id_d; end
     end
   end
   
-  if (COMB) begin: gen_comb_wake
+  if (COMB) begin: gen_comb_wake_id
     assign wake = &sync_present_d;
+    assign id   = id_d;
   end else begin: gen_seq_wake
     assign wake = &sync_present_q;
+    assign id   = id_q;
   end
 
 /*******************************************************/
