f0ad4600cd1a77aa501659be030bb71a948e34fc
Victor Isachi <victor.isachi@unibo.it>
2025-06-21 09:50:53 +0200

    Improved heighbor node id and error handling

--- hw/fractal_sync_neighbor.sv
+++ hw/fractal_sync_neighbor.sv
@@ -60,7 +60,10 @@ module fractal_sync_neighbor
   logic               clear_sync_req;
   logic[N_PORTS-1:0]  sync_present_d, sync_present_q;
   logic               wake;
-  logic[NBR_ID_W-1:0] id_d, id_q, id;
+  logic[NBR_ID_W-1:0] id_d[N_PORTS];
+  logic[NBR_ID_W-1:0] id_q[N_PORTS];
+  logic[NBR_ID_W-1:0] id[N_PORTS];
+  logic               same_id;
 
 /*******************************************************/
 /**                Internal Signals End               **/
@@ -70,32 +73,54 @@ module fractal_sync_neighbor
 
   for (genvar i = 0; i < N_PORTS; i++) begin: gen_sync_req_rsp
     assign sync_req[i]      = req_i[i].sync;
+    assign id_d[i]          = sync_req[i] ? req_i[i].sig.id : '0;
     assign rsp_o[i].wake    = wake;
     assign rsp_o[i].sig.lvl = 1'b1;
-    assign rsp_o[i].sig.id  = id;
-    assign rsp_o[i].error   = 1'b0;
+    assign rsp_o[i].sig.id  = (wake & same_id) ? id[0] : '0;
+    assign rsp_o[i].error   = (wake & same_id) ? 1'b0  : 1'b1;
   end
 
   assign clear_sync_req = &sync_present_q;
 
   assign sync_present_d = sync_present_q | sync_req;
-  // No id checking is done: it is assumed that both requests will have the same id (this should be enforced by the tile-FS interface module)
-  assign id_d           = req_i[0].sig.id;
 
-  always_ff @(posedge clk_i, negedge rst_ni) begin: presence_id_tracker
-    if (!rst_ni)          begin sync_present_q <= '0;             id_q <= 0;    end
+  always_ff @(posedge clk_i, negedge rst_ni) begin: presence_tracker
+    if (!rst_ni)          sync_present_q <= '0;
     else begin
-      if (clear_sync_req) begin sync_present_q <= '0;             id_q <= 0;    end
-      else                begin sync_present_q <= sync_present_d; id_q <= id_d; end
+      if (clear_sync_req) sync_present_q <= '0;
+      else                sync_present_q <= sync_present_d;
     end
   end
+
+  for (genvar i = 0; i < N_PORTS; i++) begin: gen_id_tracker
+    always_ff @(posedge clk_i, negedge rst_ni) begin
+      if (!rst_ni)          id_q[i] <= '0;
+      else begin
+        if (clear_sync_req) id_q[i] <= '0;
+        else                id_q[i] <= id_d[i];
+      end
+    end
+  end
+
+  always_comb begin: same_id_logic
+    same_id = 1'b1;
+    for (int unsigned i = 1; i < N_PORTS; i++)
+      if (id[i] != id[i-1])
+        same_id = 1'b0;
+  end
   
-  if (COMB) begin: gen_comb_wake_id
-    assign wake = &sync_present_d;
-    assign id   = id_d;
+  for (genvar i = 0; i < N_PORTS; i++) begin: gen_id
+    if (COMB) begin: gen_comb_id
+      assign id[i] = id_d[i];
+    end else begin: gen_seq_id
+      assign id[i] = id_q[i];
+    end
+  end
+
+  if (COMB) begin: gen_comb_wake
+    assign wake  = &sync_present_d;
   end else begin: gen_seq_wake
-    assign wake = &sync_present_q;
-    assign id   = id_q;
+    assign wake  = &sync_present_q;
   end
 
 /*******************************************************/
